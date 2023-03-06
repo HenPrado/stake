67a42280417ab0e62550fb64df3c02d2d8695dbe
HenPrado <44711096+HenPrado@users.noreply.github.com>
2023-03-06 14:25:04 -0300

    Update mapping.ts

--- api/mapping.ts
+++ api/mapping.ts
@@ -219,6 +219,6 @@ export const stakePoolsWithHostnames: {
   },
   {
     stakePoolAddress: 'Bb76RqtkFyK9KQNaUBQ4DHF21opK83rs1KVuky3tAwAM',
-    hostname: 'stake.cyberskies.io',
+    hostname: 'https://stake.cyberskies.io',
   },
 ]
